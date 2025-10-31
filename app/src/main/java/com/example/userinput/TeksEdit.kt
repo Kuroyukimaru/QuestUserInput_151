394cbd8fb6720917012ac9e4e7869d3070a9e2f5
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-10-31 09:55:27 +0700

    membuat divider di bawah tombol submit

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -95,7 +95,14 @@ fun FormDataDiri(modifier: Modifier){
             Text(stringResource(R.string.submit))
         }
 
-
+        Divider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium)
+            ),
+            thickness = dimensionResource(R.dimen.padding_tipis),
+            color = Color.DarkGray
+        )
     }
 }
 
