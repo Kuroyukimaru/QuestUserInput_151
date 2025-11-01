e2808a119685c21c7401b498924c1c04c8980eec
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-01 20:56:05 +0700

    buat menyimpan input pengguna

--- app/src/main/java/com/example/userinput/pendaftaran.kt
+++ app/src/main/java/com/example/userinput/pendaftaran.kt
@@ -22,5 +22,10 @@ fun FormPendaftaran(modifier: Modifier = Modifier) {
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
 
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var statusPerkawinan by remember { mutableStateOf("") }
+
     
 }
