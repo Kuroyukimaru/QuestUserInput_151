3dd3e6afbab66d7b53ff76a7d5bf943ae3086bc2
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-01 20:54:14 +0700

    buat var nama, alamat, jenis kelamin, dan status

---
+++ app/src/main/java/com/example/userinput/pendaftaran.kt
@@ -0,0 +1,26 @@
+package com.example.userinput
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FormPendaftaran(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
+    
+}
